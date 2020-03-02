90d6ca7e9cba2f6fe83f5525abbc1a5caa97c1e4
yusuf <ybasori@gmail.com>
2020-03-02 21:50:05 +0700

    second commit

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,10 +1,22 @@
-import React, {Component} from 'react';
+import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom';
-import Header from './layout/Header'
+import Header from './layout/Header';
+import Dashboard from './leads/Dashboard';
+import { Provider } from 'react-redux';
+import store from '../store';
 
-class App extends Component{
-    render(){
-        return (<Header />)
+class App extends Component {
+    render() {
+        return (
+            <Provider store={store}>
+                <Fragment>
+                    <Header />
+                    <div className="container">
+                        <Dashboard />
+                    </div>
+                </Fragment>
+            </Provider>
+        )
     }
 }
 
